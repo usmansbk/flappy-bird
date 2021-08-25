46f11579768a812692afae9922f97a0be314005f
Usman Suleiman <usmansbk@gmail.com>
2021-08-25 11:11:30 +0100

    Load assets

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -6,30 +6,10 @@ export default class GameScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.setBaseURL('http://labs.phaser.io');
-
-    this.load.image('sky', 'assets/skies/space3.png');
-    this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-    this.load.image('red', 'assets/particles/red.png');
+    this.load.image('0', '/assets/sprites/0.png');
   }
 
   create() {
-    this.add.image(400, 300, 'sky');
-
-    const particles = this.add.particles('red');
-
-    const emitter = particles.createEmitter({
-      speed: 100,
-      scale: { start: 1, end: 0 },
-      blendMode: 'ADD',
-    });
-
-    const logo = this.physics.add.image(400, 100, 'logo');
-
-    logo.setVelocity(100, 200);
-    logo.setBounce(1, 1);
-    logo.setCollideWorldBounds(true);
-
-    emitter.startFollow(logo);
+    this.add.image(100, 100, '0');
   }
 }
