c7c5abf8b2d2387d101c5adc731fd73ddad6b230
Usman Suleiman <usmansbk@gmail.com>
2021-08-28 21:06:52 +0100

    Fix high score

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -161,7 +161,7 @@ export default class GameScene extends Phaser.Scene {
       const currentBest = localStorage.getItem(BEST_SCORE_KEY) || 0;
       const bestScore = Math.max(currentBest, this.score);
       localStorage.setItem(BEST_SCORE_KEY, Math.max(this.score, bestScore));
-      this.bestScoreText.setText(`High Score : ${bestScore}`);
+      this.bestScoreText.setText(`High Score : ${currentBest}`);
 
       this.slideButton();
     }
